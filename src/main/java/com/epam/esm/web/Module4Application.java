328202d8a23c69635a81e90d177624552c5b976e
SergeiKlimovich <140184sk@gmail.com>
2021-08-19 23:40:32 +0300

    19.08 commit

--- src/main/java/com/epam/esm/web/Module4Application.java
+++ src/main/java/com/epam/esm/web/Module4Application.java
@@ -2,12 +2,19 @@ package com.epam.esm.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.epam.esm"})
 @SpringBootApplication
-public class Module4Application {
+public class Module4Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Module4Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Module4Application.class);
     }
 }
